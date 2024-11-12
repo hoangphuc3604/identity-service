756bec098bceab5047281d585d25bd70439405ef
hoangphuc3604 <nvhphuc364@gmail.com>
2024-11-12 18:06:29 +0700

    securiry config

--- src/main/java/com/springproject/identityservice/config/SecurityConfig.java
+++ src/main/java/com/springproject/identityservice/config/SecurityConfig.java
@@ -1,15 +1,25 @@
 package com.springproject.identityservice.config;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final String[] PUBLIC_ENDPOINT = { "/users", "/auth/login", "/auth/introspect" };
+    @Value("${jwt.signerKey}")
+    private String signerKey;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,12 +27,20 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINT).permitAll()
                         .anyRequest().authenticated());
-        // .formLogin(Customizer.withDefaults())
-        // .httpBasic(Customizer.withDefaults());
+
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(t -> t.decoder(jwtDecoder())));
 
         return http.build();
     }
 
+    @Bean
+    JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS256");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
 }
